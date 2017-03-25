b63d956c9aa80d0bfa53d08613a07364dc37e514
dvprabhu1995 <dvprabhu1995@gmail.com>
2017-03-25 18:49:43 +0530

    Tested math.c successfully

--- math.c
+++ math.c
@@ -4,11 +4,11 @@ int math(int a, int b, char ch)
 {
   switch(ch)
   {
-   case "+" : return (a+b);
+   case '+' : return (a+b);
               break;
-   case "-" : return (a-b);
+   case '-' : return (a-b);
               break;
-   case "*" : return a * b;
+   case '*' : return a * b;
               break;              
   }
 }
